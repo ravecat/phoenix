819651ac98f9cbc61fdda0a18ff43ef873533742
Max Sharov <maxsharoff@gmail.com>
2026-05-15 00:55:40 +0200

    build: commit release artifacts

--- release.config.mjs
+++ release.config.mjs
@@ -2,5 +2,25 @@ import process from "node:process"
 
 export default {
   branches: [process.env.DEFAULT_BRANCH || "master", { name: "beta", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle: "# Changelog",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "pnpm-lock.yaml"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
   tagFormat: "${version}",
 }
